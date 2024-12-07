555e9d25688269a665920063cb0c3a64ff5cda02
Luke Petschauer <luke@lukewrites.com>
2024-12-06 21:51:50 -0800

    finish day 1, O(n log n), O(n)

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,9 @@
+from collections import Counter
+
 from typing import List
 
 
-def find_sum_of_differences(file_path:str):
+def find_sum_of_differences(file_path: str):
     two_lists = zip_up_the_two_lists(file_path)
     left, right = sort_lists(two_lists)
     sum_of_differences = 0
@@ -9,7 +11,8 @@ def find_sum_of_differences(file_path:str):
         sum_of_differences += abs(left[i] - right[i])
     return sum_of_differences
 
-def zip_up_the_two_lists(file_path:str):
+
+def zip_up_the_two_lists(file_path: str):
     with open(file_path, 'r') as f:
         lines = f.readlines()
         left, right = [], []
@@ -19,8 +22,21 @@ def zip_up_the_two_lists(file_path:str):
             right.append(int(split_line[1]))
     return [left, right]
 
-def sort_lists(lists:List[List[int]]):
+
+def sort_lists(lists: List[List[int]]):
     return [sorted(l) for l in lists]
+
+
+def return_frequency_score(file_path: str):
+    two_lists = zip_up_the_two_lists(file_path)
+    left, right = two_lists[0], two_lists[1]
+    frequency_score = 0
+    right_counter = Counter(right)
+    for num in left:
+        frequency_score += num * right_counter.get(num, 0)
+    return frequency_score
+
 
 if __name__ == '__main__':
     print(find_sum_of_differences('day1/day1_input.txt'))
+    print(return_frequency_score('day1/day1_input.txt'))
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -1,4 +1,4 @@
-from day1 import find_sum_of_differences, zip_up_the_two_lists, sort_lists
+from day1 import find_sum_of_differences, zip_up_the_two_lists, sort_lists, return_frequency_score
 
 
 def test_zip_up_the_two_lists_returns_correct_lists():
@@ -6,8 +6,14 @@ def test_zip_up_the_two_lists_returns_correct_lists():
     assert two_lists[0] == [3, 4, 2, 1, 3, 3]
     assert two_lists[1] == [4, 3, 5, 3, 9, 3]
 
+
 def test_sort_lists():
     assert sort_lists([[3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]]) == [[1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9]]
 
+
 def test_find_sum_of_differences():
     assert find_sum_of_differences('day1/day_test_input.txt') == 11
+
+
+def test_frequency_score():
+    return_frequency_score('day1/day_test_input.txt') == 32
